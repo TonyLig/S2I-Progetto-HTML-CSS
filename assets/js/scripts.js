1f4b7cb1baaa2197a263dcfa2733746a8746f65f
TonyLig <118136260+TonyLig@users.noreply.github.com>
2023-03-31 20:48:05 +0200

    modifikata sezione skills

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+// Leaflet Map
+var map = L.map('map').setView([40.847, 14.265], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
