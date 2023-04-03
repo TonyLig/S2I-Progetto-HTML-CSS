66b2c4c8af407c9b30ee2684e7e2a1176f5b7e55
TonyLig <118136260+TonyLig@users.noreply.github.com>
2023-04-03 11:28:05 +0200

    modifiva definitiva sezione dicono di me e contatti

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,9 @@
 // Leaflet Map
-var map = L.map('map').setView([40.847, 14.265], 13);
+var map = L.map('map').setView([45.6989, 9.67], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([45.6989, 9.67]).addTo(map);
